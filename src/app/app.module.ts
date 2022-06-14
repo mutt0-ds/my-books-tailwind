79bd3d2298866225612f203964fce9dee55fdd0c
mutt0-ds <davide.muttoni@gmail.com>
2022-06-14 20:29:21 +0200

    deploy and edited tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AngularTiltModule } from 'angular-tilt';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { TooltipDirective } from './directives/tooltip.directive';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 @NgModule({
@@ -13,6 +14,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent,
     HeaderComponent,
     BooksComponent,
+    TooltipDirective,
   ],
   imports: [
     BrowserModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,12 +6,15 @@ import { BookService } from '../books/books.service';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  selectedIcon: string = 'home';
+  selectedIcon: string = 'all';
 
   constructor(private bookService: BookService) {}
 
   filterBooks(filter: string[] | string) {
     // TODO: fare qua il collegamento alle icone per il selected e cambiare nome my books
+    if (typeof filter == 'string') this.selectedIcon = filter;
+    else this.selectedIcon = filter[0];
+    console.log(this.selectedIcon);
     this.bookService.filterBooks(filter);
   }
 }
